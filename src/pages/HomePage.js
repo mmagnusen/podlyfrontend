c4bfee124540ced7304d00f5bb56894f9070b956
Marilyn Magnusen <m-mags@hotmail.co.uk>
2019-03-23 15:43:25 +0000

    head title tag

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Header, Home } from '../../src/components/';
+import { Helmet } from "react-helmet";
 
 class HomePage extends Component {
 
   render() {
     return (
         <div className="App">
+            <Helmet>
+                <title>Podcast guests</title>
+            </Helmet>
             <Header />
             <Home />
         </div>
--- src/pages/PodcastPage.js
+++ src/pages/PodcastPage.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { Header, PodcastContent } from '../../src/components/';
+import {Helmet} from "react-helmet";
 
 class PodcastPage extends Component {
 
   render() {
     return (
         <div className="App">
+        <Helmet>
+          <title>Podcast guests</title>
+        </Helmet>
             <Header />
             <PodcastContent />
         </div>
