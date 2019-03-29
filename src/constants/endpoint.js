12826a75ff3b096790c0578c074fe5cdc84a0dce
Marilyn Magnusen <m-mags@hotmail.co.uk>
2019-03-29 21:36:08 +0000

    production endpoint

--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -1,4 +1,4 @@
-export const ENDPOINT = 'http://127.0.0.1:8000'
+export const ENDPOINT = 'https://marilynmags.pythonanywhere.com'
 
 //local = 'http://127.0.0.1:8000'
 //production: 'https://marilynmags.pythonanywhere.com'
