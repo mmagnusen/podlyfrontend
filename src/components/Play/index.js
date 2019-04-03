ca8b48b9bff67f1290679a5979c2980569e232c1
Marilyn Magnusen <m-mags@hotmail.co.uk>
2019-04-04 00:25:06 +0100

    truncate snippet

--- src/components/Play/index.js
+++ src/components/Play/index.js
@@ -5,12 +5,15 @@ import mp3 from '../../resources/lay.mp3'
 import ListItem from './ListItem'
 import axios from 'axios'
 import { ENDPOINT } from '../../constants'
+import moment from 'moment'
+import Truncate from 'react-truncate';
 import './Play.scss'
 
 class Play extends Component {
 
     state = {
-        podcast: {}
+        podcast: {},
+        play: false,
     }
 
     componentDidMount() {
@@ -26,39 +29,83 @@ class Play extends Component {
 
       }
 
+      static audio = document.getElementById("player");  
+
+      togglePlayer = () => {
+
+        const audio = document.getElementById("player");  
+        
+        if (this.state.play === true ) {
+
+            this.setState({
+                play: false
+            })
+
+            audio.pause()
+
+        } else {
+
+            this.setState({
+                play: true
+            })   
+
+            audio.play()
+
+        }
+      }
+
     render() {
 
     const { episodes } = this.props
-    const { podcast } = this.state.podcast
-
-    console.log(this.state)
-
-
-
-    console.log('from play', this.props)
+    const { podcast, snippet, image, publish_date } = this.state.podcast
 
         return (
             <section className='Play'>
                 <section className='Play-inner'>
                     <section className='Play-info' >
-                        <img src="https://picsum.photos/200/300" alt='hosts'/>
+                        <section className='Play-infoImage'>
+                            <img src={`${ENDPOINT}/media/${image}`} />
+                        </section>
                         <div>
                             <h3>{podcast}</h3>
-                            <p>Abadesi, Michael</p>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting 
-                                industry. Lorem Ipsum has been the industry's standard dummy text 
-                                ever since the 1500s, when an unknown printer took a galley of 
-                                type and scrambled it to make a type specimen book. It has survived 
-                                not only five centuries, but also the leap into electronic 
-                                typesetting, remaining essentially unchanged. It was popularised 
-                                in the 1960s with the release of Letraset sheets containing Lorem 
-                                Ipsum passages, and more recently with desktop publishing software 
-                                like Aldus PageMaker including versions of Lorem Ipsum.
-                            </p>
+                            <Truncate lines={3} ellipsis={<span>...</span>}>
+                                {snippet}
+                            </Truncate>
+                            <p>{moment(publish_date).format("Do MMM YYYY") }</p>
                         </div>
                     </section>
+
                     <section className='Play-player'>
-                        <audio controls>
+                        <section className='Play-playerInner'>
+                            <section className='Play-playerControls'>
+                                <div>
+                                    <i className="fas fa-step-backward"></i>
+                                </div>
+                                <div>
+                                    {
+                                        this.state.play === false ?
+                                        <i className="fas fa-play circle" onClick={this.togglePlayer}></i>
+                                        :
+                                        <i className="fas fa-pause circle" onClick={this.togglePlayer}></i>
+                                    }
+                                </div>
+                                <div>
+                                    <i className="fas fa-step-forward"></i>
+                                </div>
+                            </section>
+                            <section className='Play-playerProgress'>
+                                <div className="ui teal progress" data-percent="74" id="example1">
+                                    <div className="bar">
+                                        <div className="progress"></div>
+                                    </div>
+                                </div>
+
+                            </section>
+                        </section>
+                    </section>
+
+                    <section className='Play-playerxxx'>
+                        <audio controls id='player'>
                             <source src={mp3} type="audio/mpeg"/>
                             Your browser does not support the audio element.
                         </audio>
